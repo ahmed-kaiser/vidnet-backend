e6662c2a573727a09f3349839fa77dd0e271db8d
Md. Kaiser Ahmed <ahmedkaiser.001@gmail.com>
2024-03-15 22:24:55 +0600

    setup project file and setup db connection

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "dotenv/config.js";
+import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
+
+const port = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error", error);
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is listening at port ${port}`);
+    });
+  })
+  .catch((error) => console.log("Database connection error", error));
