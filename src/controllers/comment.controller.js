0d41d9aa3e89f2c34496172a48de83d3dc2f6fce
Md. Kaiser Ahmed <ahmedkaiser.001@gmail.com>
2024-04-04 19:56:35 +0600

    add getVideoComments controller logic

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,9 +5,66 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-  //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID");
+  }
+
+  if (isNaN(parseInt(page)) || isNaN(parseInt(limit))) {
+    throw new ApiError(400, "Invalid page or limit value");
+  }
+
+  page = Math.max(1, parseInt(page));
+  limit = Math.max(1, parseInt(limit));
+
+  const skip = Math.max(0, (page - 1) * limit);
+
+  let comments;
+  try {
+    comments = await Comment.aggregate([
+      {
+        $match: { video: videoId },
+      },
+      {
+        $lookup: {
+          from: "users",
+          foreignField: "_id",
+          localField: "user",
+          as: "user",
+          pipeline: [
+            {
+              $project: {
+                fullName: 1,
+                avater: 1,
+                createdAt: 1,
+              },
+            },
+            {
+              $addFields: {
+                user: {
+                  $first: "$user",
+                },
+              },
+            },
+          ],
+        },
+      },
+      {
+        $skip: skip,
+      },
+      {
+        $limit: limit,
+      },
+    ]);
+  } catch (error) {
+    throw new ApiError(500, "Server error");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { comments }, "Comments fetched successfully"));
 });
 
 const addComment = asyncHandler(async (req, res) => {
